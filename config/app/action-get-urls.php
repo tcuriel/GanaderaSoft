8fb058eeb99ea5dfa1ee31625d2d1a229bb41a5c
tcuriel <tcurielenator@gmail.com>
2024-07-27 10:31:20 -0400

    Modificar archivo en config.app

--- config/app/action-get-urls.php
+++ config/app/action-get-urls.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'find_farm' => '/GanaderoSoft/Finca/finca/',
-    'find_farmFUllData' => '/GanaderoSoft/Finca/fincafulldata/',
-    'list_rebano' => '/GanaderoSoft/Finca/listar-rebano/',
-    'get_rebano' => '/GanaderoSoft/Finca/get-rebano/',
-    'list_personal' => '/GanaderoSoft/Finca/listar-personal/',
-    'get_personal' => '/GanaderoSoft/Finca/get-personal/',
-    'list_inventario' => '/GanaderoSoft/Finca/listar-inventario-',
-    'get_inventario' => '/GanaderoSoft/Finca/inventario-',
-    'list_animal' => '/GanaderoSoft/Animal/listar-animales/',
+    'find_farm' => '/Finca/finca/',
+    'find_farmFUllData' => '/Finca/fincafulldata/',
+    'list_rebano' => '/Finca/listar-rebano/',
+    'get_rebano' => '/Finca/get-rebano/',
+    'list_personal' => '/Finca/listar-personal/',
+    'get_personal' => '/Finca/get-personal/',
+    'list_inventario' => '/Finca/listar-inventario-',
+    'get_inventario' => '/Finca/inventario-',
+    'list_animal' => '/Animal/listar-animales/',
 ];
