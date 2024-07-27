1a1b6a3928077bcd1035126a885d3cf1fcbe81b3
tcuriel <tcurielenator@gmail.com>
2024-07-27 10:34:13 -0400

    Modificar otro archivo en config.app

--- config/app/action-post-urls.php
+++ config/app/action-post-urls.php
@@ -2,12 +2,12 @@
 
 return [
     //Crear finca
-    'create_farm' => '/GanaderoSoft/Finca/crear-finca/',
-    'update_farm' => '/GanaderoSoft/Finca/modificar-finca/,/GanaderoSoft/Finca/modificar-hierro/,/GanaderoSoft/Finca/modificar-terreno/',
-    'add_renbano_farm' => '/GanaderoSoft/Finca/crear-rebano/',
-    'add_personal_farm' => '/GanaderoSoft/Finca/ingresar-personal/',
-    'update_personal_farm' => '/GanaderoSoft/Finca/actualizar-informacion/',
-    'add_inventario_farm' => '/GanaderoSoft/Finca/inventario-',
-    'create_animal' => '/GanaderoSoft/Animal/agregar-animal/',
-    'up_file_animal' => '/GanaderoSoft/Animal/csv-agregar'
+    'create_farm' => '/Finca/crear-finca/',
+    'update_farm' => '/Finca/modificar-finca/,/GanaderoSoft/Finca/modificar-hierro/,/GanaderoSoft/Finca/modificar-terreno/',
+    'add_renbano_farm' => '/Finca/crear-rebano/',
+    'add_personal_farm' => '/Finca/ingresar-personal/',
+    'update_personal_farm' => '/Finca/actualizar-informacion/',
+    'add_inventario_farm' => '/Finca/inventario-',
+    'create_animal' => '/Animal/agregar-animal/',
+    'up_file_animal' => '/Animal/csv-agregar'
 ];
